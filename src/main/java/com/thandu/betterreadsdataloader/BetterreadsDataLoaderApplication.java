09cd83cc2c2310ed75a8e690e4196548f00e8e0a
EMERA\AP260 <sandeep.thandu@nspower.ca>
2021-12-09 14:28:04 -0400

    Setting up Connection with Cassandra

---
+++ src/main/java/com/thandu/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -0,0 +1,30 @@
+package com.thandu.betterreadsdataloader;
+
+import java.nio.file.Path;
+
+import com.thandu.betterreadsdataloader.connection.DataStaxAstraProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
+public class BetterreadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+		
+		
+	}
+
+}
---
+++ src/test/java/com/thandu/betterreadsdataloader/BetterreadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thandu.betterreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
